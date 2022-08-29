1be96e9cb4f6f9fa789d9d292d955e126d339a0e
Aditya Sood <aditya.sood10@gmail.com>
2022-08-29 14:53:41 +0530

    2nd Commit

--- factorial.cpp
+++ factorial.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int factorial(int n){
     if (n<0) {
-        cout << "Enter a positive number" << endl;
+        cout << "Enter a positive number(>=0)" << endl;
         exit(1);
     }
-    if(n!=1){
-	return(n * factorial(n-1));
+    // if(n!=1){
+	// return(n * factorial(n-1));
+    // }
+    // else return 1;
+    if (n==1) return 1;
+    else {
+        return (n * factorial(n-1));
     }
-    else return 1;
 }
