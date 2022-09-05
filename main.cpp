7bb0d423b35977435e6f7a1f2acc83bc2aef80b5
Aditya Sood <aditya.sood10@gmail.com>
2022-09-05 15:05:41 +0530

    Step-2

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 int main(){
     print_hello();
-    cout<< endl;
+    //cout<< endl;
+    printf("\n");
     int x;
     cin >> x;
-    cout<<"The factorial of"<<x<< " is "<< factorial(x)<< endl;
+    //cout<<"The factorial of"<<x<< " is "<< factorial(x)<< endl;
+    printf("The factorial of %d is %d\n", x, factorial(x));
+
     return 0;
 }
