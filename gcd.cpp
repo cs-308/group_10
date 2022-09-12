121ae331918f06fdf296c77e7db1c7c189ca935b
AndreTh0mas <harshkumariitmandi@gmail.com>
2022-09-12 18:10:47 +0530

    Step 3

--- gcd.cpp
+++ gcd.cpp
@@ -4,14 +4,33 @@
 #include <iostream>
 using namespace std;
 // Function to return gcd of a and b
+// int gcd(int a, int b)
+// {
+// 	int result = min(a, b); // Find Minimum of a nd b
+// 	while (result > 0) {
+// 		if (a % result == 0 && b % result == 0) {
+// 			break;
+// 		}
+// 		result--;
+// 	}
+// 	return result; // return gcd of a nd b
+// }
+
+// Recursive function to return gcd of a and b
 int gcd(int a, int b)
 {
-	int result = min(a, b); // Find Minimum of a nd b
-	while (result > 0) {
-		if (a % result == 0 && b % result == 0) {
-			break;
-		}
-		result--;
-	}
-	return result; // return gcd of a nd b
+    // Everything divides 0
+    if (a == 0)
+       return b;
+    if (b == 0)
+       return a;
+  
+    // base case
+    if (a == b)
+        return a;
+  
+    // a is greater
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
 }
