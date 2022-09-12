8b522c6e22aaf98387a33ab66f01488b2d1a8b94
AndreTh0mas <harshkumariitmandi@gmail.com>
2022-09-12 18:08:24 +0530

    first commit

---
+++ gcd.cpp
@@ -0,0 +1,17 @@
+// C++ program to find GCD of two numbers
+#include "functions.h"
+
+#include <iostream>
+using namespace std;
+// Function to return gcd of a and b
+int gcd(int a, int b)
+{
+	int result = min(a, b); // Find Minimum of a nd b
+	while (result > 0) {
+		if (a % result == 0 && b % result == 0) {
+			break;
+		}
+		result--;
+	}
+	return result; // return gcd of a nd b
+}
